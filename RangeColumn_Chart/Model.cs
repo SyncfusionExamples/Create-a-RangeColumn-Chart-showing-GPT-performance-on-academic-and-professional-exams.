48218575d8b91df88ddb2767aa774ae8f6515f84
MoneeshramDhanabal <moneeshram.dhanabal@syncfusion.com>
2023-06-20 11:45:45 +0530

    Made review changes mentioned

---
+++ RangeColumn_Chart/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamScoreChart
+{
+
+    public class Model
+    {
+        private string _subject;
+        private string _examType;
+        private string _examName;
+
+        public string ExamName
+        {
+            get { return _examName; }
+        }
+
+        public string Subject
+        {
+            get { return _subject; }
+            set
+            {
+                _subject = value;
+                _examName = _subject + "\n" + _examType;
+            }
+        }
+
+        public string? ExamType
+        {
+            get { return _examType; }
+            set
+            {
+                _examType = value;
+                _examName = _subject + "\n" + _examType;
+            }
+        }
+
+        public double ChatGPT_4 { get; set; }
+        public double ChatGPT_35 { get; set; }
+    }
+}
